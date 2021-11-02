8dd0ad7d263f2f30f8b9ddf9b19ab4e77db167a3
DragoAl <88578768+DragoAl@users.noreply.github.com>
2021-11-02 16:13:26 +0100

    logic js

---
+++ script.js
@@ -0,0 +1,13 @@
+// CAMPOMINATO
+// L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata,
+// in cui ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+// Quando l’utente clicca su ogni cella, 
+// la cella cliccata si colora di azzurro (o simili, l’importante è dare all’utente il feedback che ha scoperto una casella che rimarrà scoperta, con il numero relativo).
+
+// 1.L'USER SELEZIONA IL LV DI DIFFICOLTA'
+// 1.1 ATTRAVERSO IL CLICK SU DEI BOTTONI VIENE SCELTO IL LV.
+// 2.IL CLICK COMPORTA L'ITERAZIONE DI UN CICLO A SECONDA DELLE CELLE SCELTE
+// 2.1 SEMPRE AL CLICK SARA' AGGIUNTA UNA CLASSE PER IDENTIFICARE LE CELLE GIà SELEZIONATE
